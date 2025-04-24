f0c363431742bf215103726ae2b84e6df4d45db1
Ryan Andrade <andmryan@gmail.com>
2025-04-24 18:20:13 -0400

    Getting it running on landing page + getting sign up/in routes working.

---
+++ server.js
@@ -0,0 +1,51 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const morgan = require("morgan");
+const session = require("express-session");
+
+const isSignedIn = require("./middleware/is-signed-in.js");
+const passUserToView = require("./middleware/pass-user-to-view.js");
+
+const authController = require("./controllers/auth.js");
+// const gardenController = require("./controllers/garden.js");
+// const plantController = require("./controllers/plant.js");
+
+const port = process.env.PORT ? process.env.PORT : "3000";
+
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on("connected", () => {
+    console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
+
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+
+app.use(
+    session({
+      secret: process.env.SESSION_SECRET,
+      resave: false,
+      saveUninitialized: true,
+    })
+);
+  
+app.get("/", (req, res) => {
+    res.render("homepage.ejs", {
+      user: req.session.user,
+    });
+});
+
+app.use(passUserToView);
+app.use("/auth", authController);
+app.use(isSignedIn);
+// app.use("/users/:userId/garden", gardenController);
+// app.use("/users/:userId/plant", plantController);
+
+app.listen(port, () => {
+    console.log(`The express app is ready on port ${port}!`);
+});
